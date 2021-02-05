09046cfefb25b5f649e8717e71b71f68f1a74423
tony.wu <tony.wu@vpon.com>
2021-02-05 18:16:17 +0800

    impl clac eland date collection name

--- service/SegmentService.py
+++ service/SegmentService.py
@@ -15,14 +15,15 @@ class GenerateSegmentService:
 
     def generate(self, config):
         log.info("generate() config={}".format(config))
-        print(self.__calc_time(config["day"]))
-        print(self.__mongo_repo.list_collection_names())
+        print(self.__calc_eland_data_collection_name_list(2))
+        # print(self.__mongo_repo.list_collection_names())
         # self.__mongo_repo.find_all_by_query_only()
         pass
 
-    #TODO
-    def __calc_time(self, days):
+    def __calc_eland_data_collection_name_list(self, days):
         one_day_second = 86400
-        today_timestamp = int(time.time()) - 2 * one_day_second
-        print(datetime.datetime.fromtimestamp(today_timestamp).strftime("%Y%m%d"))
-        return datetime.datetime.fromtimestamp(today_timestamp - days * one_day_second).strftime("%Y%m%d")
+        from_timestamp = int(time.time()) - 2 * one_day_second
+        to_timestamp_str = datetime.datetime.fromtimestamp(from_timestamp - days * one_day_second).strftime(
+            "%Y%m%d")
+        from_timestamp_str = datetime.datetime.fromtimestamp(from_timestamp).strftime("%Y%m%d")
+        return [str(date) for date in range(int(to_timestamp_str), int(from_timestamp_str))]
