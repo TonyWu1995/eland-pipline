06c5d94dfddeb129ea338c496c4876dd89bd827e
tony.wu <tony.wu@vpon.com>
2021-02-17 11:19:33 +0800

    impl mongodb test

--- test/test_eland_mongo_service.py
+++ test/test_eland_mongo_service.py
@@ -19,32 +19,32 @@ class TestElandDataMongoService(TestCase):
         QuerySet(ElandDataDocument,
                  ElandDataDocument().switch_collection("test_aggregate")._get_collection()).delete()
 
-
     def __save(self):
-        timestamp = int(time.time()) - 2 * 86400
-        ElandDataDocument(
-            uuid="test",
-            location=[LocationDocument(region="新加坡", percentage=0)],
-            platform=["Mobile"],
-            browser=["Line"],
-            os=["iOS"],
-            gender_tag=Gender.FEMALE,
-            geo_tag=Geo.TAIWAN,
-            age_tag=Age.TWENTY_FIVE_THIRTY_FOUR,
-            income_tag=Income.ONE_HUNDRED,
-            create_at=123,
-            update_at=timestamp,
-            sex=[48.81, 51.19],
-            income=[47.77, 23.21, 16.13, 12.89],
-            age=[33.0, 22.89, 14.45, 11.53, 18.13],
-            habit_weekday=[HabitWeekDayDocument(weekday="Mon", percentage=16.13)],
-            habit_hour=[HabitHourDocument(hour=0, percentage=1)],
-            month_usage_score=100,
-            interest=[InterestDocument(tag="族群:健身族",
-                                       score=1)],
-            intent=["室內娛樂:DIY手作/手工藝", "軍事:軍事"]
-        ).switch_collection("test_aggregate") \
-            .save()
+        for i in range(2):
+            timestamp = int(time.time()) - 2 * 86400
+            print(ElandDataDocument(
+                uuid=str(i),
+                location=[LocationDocument(region="新加坡", percentage=0)],
+                platform=["Mobile"],
+                browser=["Line"],
+                os=["iOS"],
+                gender_tag=Gender.FEMALE,
+                geo_tag=Geo.TAIWAN,
+                age_tag=Age.TWENTY_FIVE_THIRTY_FOUR,
+                income_tag=Income.ONE_HUNDRED,
+                create_at=123,
+                update_at=timestamp,
+                sex=[48.81, 51.19],
+                income=[47.77, 23.21, 16.13, 12.89],
+                age=[33.0, 22.89, 14.45, 11.53, 18.13],
+                habit_weekday=[HabitWeekDayDocument(weekday="Mon", percentage=16.13)],
+                habit_hour=[HabitHourDocument(hour=0, percentage=1)],
+                month_usage_score=100,
+                interest=[InterestDocument(tag="族群:健身族",
+                                           score=i + 1)],
+                intent=["室內娛樂:DIY手作/手工藝", "軍事:軍事"]
+            ).switch_collection("test_aggregate") \
+                  .save())
 
     def test_list_collection_names_have_value(self):
         config = MongoDBConfig(database="test", host="mongomock://localhost", port=27017, username=None, password=None,
@@ -62,7 +62,7 @@ class TestElandDataMongoService(TestCase):
                                collection_name="eland_data")
         service = ElandDataMongoService(config)
 
-        result = service.find_all_by_query_only(None, Q(**{"interest__tag": "族群:健身族"}), *['uuid'])
+        result = service.find_all_by_query_only(None, Q(**{"interest__tag": "族群:健身族"}), 'uuid')
 
         self.assertEqual(len(result), 0)
 
@@ -72,9 +72,9 @@ class TestElandDataMongoService(TestCase):
         service = ElandDataMongoService(config)
         self.__save()
 
-        result = service.find_all_by_query_only("test_aggregate", Q(**{"interest__tag": "族群:健身族"}), *['uuid'])
+        result = service.find_all_by_query_only("test_aggregate", Q(**{"interest__tag": "族群:健身族"}), 'uuid')
 
-        self.assertEqual(len(result), 1)
+        self.assertEqual(len(result), 2)
 
         self.__delete()
 
@@ -82,7 +82,31 @@ class TestElandDataMongoService(TestCase):
         config = MongoDBConfig(database="test", host="mongomock://localhost", port=27017, username=None, password=None,
                                collection_name="eland_data")
         service = ElandDataMongoService(config)
+        self.__save()
 
         result = service.find_all_by_query_only("test_aggregate", None, "uuid")
 
-        self.assertEqual(len(result), 0)
+        self.assertEqual(len(result), 2)
+        self.__delete()
+
+    def test_find_max_value_by_tag_name_asc(self):
+        config = MongoDBConfig(database="test", host="mongomock://localhost", port=27017, username=None, password=None,
+                               collection_name="eland_data")
+        service = ElandDataMongoService(config)
+        self.__save()
+
+        result = service.find_max_value_by_tag_name("test_aggregate", Q(**{"interest__tag": "族群:健身族"}),
+                                                    'interest__score')
+        self.assertEqual(result.interest[0].score, 1)
+        self.__delete()
+
+    def test_find_max_value_by_tag_name_desc(self):
+        config = MongoDBConfig(database="test", host="mongomock://localhost", port=27017, username=None, password=None,
+                               collection_name="eland_data")
+        service = ElandDataMongoService(config)
+        self.__save()
+
+        result = service.find_max_value_by_tag_name("test_aggregate", Q(**{"interest__tag": "族群:健身族"}),
+                                                    'interest__score', True)
+        self.assertEqual(result.interest[0].score, 2)
+        self.__delete()
